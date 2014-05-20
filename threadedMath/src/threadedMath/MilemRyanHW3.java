7a0cca618325416e475d480d8d93b7af7ac6006d
Ryan Milem <gint271@gmail.com>
2014-05-20 16:58:21 -0700

    Can now take numbers from user.

--- threadedMath/src/threadedMath/MilemRyanHW3.java
+++ threadedMath/src/threadedMath/MilemRyanHW3.java
@@ -11,12 +11,39 @@
 
 package threadedMath;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class MilemRyanHW3 
 {
 
 	public static void main(String[] args) 
 	{
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+		String[] input;
+		String split;
+		int number1;
+		int number2;
 		
+		while(true)
+		{
+			System.out.println("Enter the two integers seperated by a space.");
+			try
+			{
+				input = keyboard.readLine().split(" ");
+			}
+			catch (Exception e)
+			{
+				System.out.println("Failed to read from console.");
+				return;
+			}
+			
+			number1 = Integer.parseInt(input[0]);
+			number2 = Integer.parseInt(input[1]);
+			
+			System.out.println("Number 1 is " + number1);
+			System.out.println("Number 2 is " + number2);
+		}
 	}
 
 }
