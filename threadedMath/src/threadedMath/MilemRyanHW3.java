7aea3a0601eb9a685a249a6e2a3fe3d8a1d0c3a9
Ryan Milem <gint271@gmail.com>
2014-05-20 16:47:49 -0700

    Initial commit.

---
+++ threadedMath/src/threadedMath/MilemRyanHW3.java
@@ -0,0 +1,22 @@
+/*
+ * Author: Ryan Milem
+ * 
+ * Homework Assignment 3
+ * 
+ * 5/20/14
+ * 
+ * Handles starting the threads to perform the various operations on the inputted numbers.  Also
+ * takes in user input.
+ */
+
+package threadedMath;
+
+public class MilemRyanHW3 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
